083608695a2a765747b56338516348bf114b8571
erikokres <56141967+erikokres@users.noreply.github.com>
2020-03-27 18:45:05 +0700

    add Empty Project

---
+++ angry bird/Assets/GameControler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControler : MonoBehaviour
+{ public SlingShooter SlingShooter;
+    public List<Bird> Birds;
+
+    void Start()
+    {
+        // for(int i = 0; i < Birds.Count; i++)
+        // {
+        //     Birds[i].OnBirdDestroyed += ChangeBird;
+        // }
+        SlingShooter.InitiateBird(Birds[0]);
+    }
+    //  public void ChangeBird()
+    // {
+    //     Birds.RemoveAt(0);
+
+    //     if(Birds.Count > 0)
+    //         SlingShooter.InitiateBird(Birds[0]);
+    // }
+}
